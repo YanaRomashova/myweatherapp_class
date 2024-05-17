4f2736181dfc1a7ee781a3784db4e21e3585eea6
Iana Khasanshina <ianakhasanshina@wifi-64-187-244-225.nyit.edu>
2024-05-17 19:33:58 -0400

    thispush

--- weather-app/src/components/LocationField.jsx
+++ weather-app/src/components/LocationField.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const citynameField = {
+  borderRadius: "15px", 
+  borderColor: "#4C4DDB",
+  width: "334px",
+  height: "42px",
+  color: "black"
+};
+
 function LocationField({ location, setLocation, searchLocation }) {
   return (
     <div className="search">
       <input
+        style={citynameField}
         value={location}
         onChange={(event) => setLocation(event.target.value)}
         onKeyPress={searchLocation}
